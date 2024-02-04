5cdd029b60de70d326613e8c15e5240039baec51
shiva avvari <aisshiva766@gmail.com>
2024-02-04 09:30:55 +0530

    adding files

---
+++ plot1.R
@@ -0,0 +1,28 @@
+# import the required library
+library(readr)
+library(lubridate)
+library(tidyr)
+#import the dataset
+household_power_consumption <- read_delim("household_power_consumption.txt", delim = ";", escape_double = FALSE, trim_ws = TRUE)
+
+# create a dataframe
+household_power_consumption <- data.frame(household_power_consumption,stringsAsFactors = FALSE)
+
+# converting the Date column from character type to Date type
+household_power_consumption$Date = as.Date(household_power_consumption$Date, format="%d/%m/%Y")
+household_power_consumption$Date_time = paste(household_power_consumption$Date,household_power_consumption$Time)
+household_power_consumption$Date_time = strptime(household_power_consumption$Date_time,format = " %Y-%m-%d %H:%M:%S" ,tz="EST ")
+
+
+
+# creating a subset of Data for specific time  period
+household_power_consumption_data <- subset(household_power_consumption, Date  > "2007-01-31"  & Date < "2007-02-03")
+# convert the Global_active_power column to numeric if it is not already
+household_power_consumption_data$Global_active_power = as.numeric(household_power_consumption_data$Global_active_power)
+
+# Open the Png device to store the data
+png(filename="plot1.png",width=480,height = 480)
+# Plot a histogram for plot1
+hist(household_power_consumption_data$Global_active_power,col= "Red", main = "Global Active Power", xlab = "Global Active Power(Kilowatts)")
+# turn off the device after saving the image in png format.
+dev.off()
